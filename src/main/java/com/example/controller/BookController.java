951ac6669b133d31cf4ebf8eaf4ef73253b9bb6f
Rafael Ferreira <rafaelferreira@mindera.com>
2020-10-22 16:29:47 +0100

    Changed structure of folders, requests now working

--- src/main/java/com/example/controller/BookController.java
+++ src/main/java/com/example/controller/BookController.java
@@ -1,8 +1,8 @@
-package controller;
+package com.example.controller;
 
 
 
-import model.Book;
+import com.example.model.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import repository.BookRepository;
+import com.example.repository.BookRepository;
 
 
 @RestController
@@ -23,7 +23,6 @@ public class BookController {
 
     @Autowired
     public BookController(BookRepository bookRepository){
-        logger.debug("OLA");
         this.bookRepository = bookRepository;
     }
 
--- src/main/java/com/example/model/Book.java
+++ src/main/java/com/example/model/Book.java
@@ -1,4 +1,4 @@
-package model;
+package com.example.model;
 
 import org.springframework.data.annotation.Id;
 
